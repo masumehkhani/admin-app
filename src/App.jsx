995cde6c70df6095e18132f9fe1b6a838ee89fc9
masumeh <khanimasume88@gmail.com>
2023-05-23 16:58:38 +0430

    Add theme and color by useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-
+import { ColorModeContext } from "./theme";
+import { CssBaseline,ThemePimport, ThemeProvider } from "@mui/material";
 function App() {
   return (
-    <>
-     
-    </>
+    <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}/>
+    <CssBaseline/>
+    <div className="app">
+        <main className="content"></main>
+    </div>
+    </ColorModeContext.Provider>
+   
   )
 }
 
